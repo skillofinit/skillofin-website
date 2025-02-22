b25e8f1a3f465067cbcf41dafe3b9772575ca84d
afriddev <afridayan01@gmail.com>
2025-02-23 03:46:17 +0530

    Update BASE_URL to use production API endpoint

--- src/utiles/appUtils.ts
+++ src/utiles/appUtils.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api";
-// export const BASE_URL = "https://skillofinapi.vercel.app/api";
+// export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://skillofinapi.vercel.app/api";
 
 
 //company urls
