1e4147294460ff5d804ebe872aedd8e418105381
SHIAK AFRID <afridayan01@gmail.com>
2025-02-25 07:55:51 +0000

    Updated protected route

--- src/features/dashboard/ProtectedRoute.tsx
+++ src/features/dashboard/ProtectedRoute.tsx
@@ -1,9 +1,29 @@
 import DashboardNavBar from "@/utils/DashboardNavBar";
 import HomeFooter from "@/utils/HomeFooter";
-import { Outlet, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, Navigate, useLocation, useNavigate } from "react-router-dom";
 
 function ProtectedLayout() {
   const emailId = localStorage.getItem("emailId");
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname) {
+      if (emailId) {
+        navigate(pathname);
+      }
+      else{
+        if(pathname === "/signup"){
+          navigate("/signup")
+        }
+        else{
+          navigate('/login')
+        }
+      }
+    }
+  }, [pathname]);
 
   if (!emailId) {
     return <Navigate to="/login" replace />;
