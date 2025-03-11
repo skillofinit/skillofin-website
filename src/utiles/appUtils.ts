069313b6ce7237811fef093e17dd81be06aebc18
Skillofin <skillofinit@gmail.com>
2025-03-12 00:58:33 +0530

    Update appUtils.ts

--- src/utiles/appUtils.ts
+++ src/utiles/appUtils.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://127.0.0.1:3000/api";
-// export const BASE_URL = "https://skillofinapis.vercel.app/api";
+// export const BASE_URL = "http://127.0.0.1:3000/api";
+export const BASE_URL = "https://skillofinapis.vercel.app/api";
 
 //company urls
 export const COMPANY_EMAIL = "contact@Skillofin.com";
